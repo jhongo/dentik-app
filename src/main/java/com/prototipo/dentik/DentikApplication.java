75003ab2b3872bc91bda5f41224bbf4b704ae080
jhongo <jhon_gom22@outlook.com>
2026-06-22 15:33:30 -0500

    🎉 [INIT]: Inicio proyecto Dentik - PISIP
    [INIT]: Inicio proyecto Dentik - PISIP

---
+++ src/main/java/com/prototipo/dentik/DentikApplication.java
@@ -0,0 +1,13 @@
+package com.prototipo.dentik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prototipo/dentik/DentikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prototipo.dentik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
